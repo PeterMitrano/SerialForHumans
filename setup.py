00125495558daf231c26b87d4a5269898068e055
Peter Mitrano <mitranopeter@gmail.com>
2017-01-21 16:35:49 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.5
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='A usable serial monitor',
+      author='Peter Mitrano',
+      author_email='mitranopeter@gmail.com',
+      url='https://github.com/petermitrano/serialforhumans',
+      packages=['serial_for_humans'],
+     )
